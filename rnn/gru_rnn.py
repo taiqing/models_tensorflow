3f90dca6207001db758bc98bbec3c17a1e872b72
Wang Taiqing <taiqingwang@creditease.cn>
2016-08-22 20:00:22 +0800

    rnn: the initial weights are critical to the optimisation of the whole network; develop two weight variable initialisation helper function to properly initialise the weights

--- rnn/gru_rnn.py
+++ rnn/gru_rnn.py
@@ -9,23 +9,45 @@ from tensorflow.python import array_ops
 
 from tensorflow.examples.tutorials.mnist import input_data
 
+
+def weight_variable_normal(shape, stddev=None):
+    if stddev is not None:
+        std = stddev
+    else:
+        std = 1.0 / np.sqrt(shape[0])
+    initial = tf.truncated_normal(shape=shape, mean=0.0, stddev=std)
+    return tf.Variable(initial)
+
+
+def weight_variable_uniform(shape, radius):
+    initial = tf.random_uniform(shape=shape, minval=-radius, maxval=radius)
+    return tf.Variable(initial)
+
+
 if __name__ == '__main__':
     n_input = 28
     n_step = 28
     n_class = 10
     n_hidden = 4 * n_input
     batch_size = 100
-    learning_rate = 1e-3
-    n_iteration = 1e6
-    valid_steps = 5000
+    learning_rate = 1e-2
+    n_iteration = 500
+    valid_steps = 100
 
     x = tf.placeholder(tf.float32, [None, n_step, n_input])
     y = tf.placeholder(tf.float32, [None, n_class])
-    W_z = tf.Variable(tf.random_normal([n_input + n_hidden, n_hidden]))
-    W_r = tf.Variable(tf.random_normal([n_input + n_hidden, n_hidden]))
-    W_c = tf.Variable(tf.random_normal([n_input + n_hidden, n_hidden]))
-    W_out = tf.Variable(tf.random_normal([n_hidden, n_class]))
+    
+    W_z = weight_variable_normal(shape=[n_input + n_hidden, n_hidden])
+    W_r = weight_variable_normal(shape=[n_input + n_hidden, n_hidden])
+    W_c = weight_variable_normal(shape=[n_input + n_hidden, n_hidden])
+    W_out = weight_variable_normal(shape=[n_hidden, n_class])
     b_out = tf.Variable(np.zeros((n_class), np.float32))
+
+    #W_z = weight_variable_uniform(shape=[n_input + n_hidden, n_hidden], radius=0.05)
+    #W_r = weight_variable_uniform(shape=[n_input + n_hidden, n_hidden], radius=0.05)
+    #W_c = weight_variable_uniform(shape=[n_input + n_hidden, n_hidden], radius=0.05)
+    #W_out = weight_variable_uniform(shape=[n_hidden, n_class], radius=0.05)
+    #b_out = tf.Variable(np.zeros((n_class), np.float32))
 
     states = []
     for t in range(n_step):
@@ -35,9 +57,11 @@ if __name__ == '__main__':
         else:
             h_prev = states[-1]
         hx = array_ops.concat(1, [h_prev, x_t])
+        # z: update gate
         z_t = tf.sigmoid(tf.matmul(hx, W_z))
+        # r: reset gate
         r_t = tf.sigmoid(tf.matmul(hx, W_r))
-        # h_c: hidden state candidate
+        # h_c: candidate hidden state
         h_c = tf.tanh(tf.matmul(array_ops.concat(1, [r_t * h_prev, x_t]), W_c))
         h_t = (1 - z_t) * h_prev + z_t * h_c
         states.append(h_t)
@@ -45,6 +69,7 @@ if __name__ == '__main__':
     pred = tf.argmax(proba, dimension=1)
     accuracy = tf.reduce_mean(tf.cast(tf.equal(pred, tf.argmax(y, dimension=1)), tf.float32))
     cost = tf.reduce_mean(tf.reduce_sum(-y * tf.log(proba), 1))
+    #train_step = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
     train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -61,15 +86,19 @@ if __name__ == '__main__':
     n_sample = train_x.shape[0]
     for i in range(int(n_iteration)):
         # validate the model
-        if i % valid_steps == 0:
-            loss, accu = sess.run([cost, accuracy], feed_dict={x: valid_x, y: valid_y})
-            print '{i} batches fed in, valid set, loss {l:.4f}, accuracy {a:.2f}%'.format(i=i, l=loss, a=accu * 100.)
+        #if i % valid_steps == 0:
+        #    loss, accu = sess.run([cost, accuracy], feed_dict={x: valid_x, y: valid_y})
+        #    print '{i} batches fed in, valid set, loss {l:.4f}, accuracy {a:.2f}%'.format(i=i, l=loss, a=accu * 100.)
         # get a batch of samples
         idx = np.random.randint(0, n_sample, batch_size)
         batch_x = train_x[idx, :, :]
         batch_y = train_y[idx, :]
         sess.run(train_step, feed_dict={x: batch_x, y: batch_y})
+        if i % 10 == 0:
+            loss, accu, state = sess.run([cost, accuracy, states[-1]], feed_dict={x: batch_x, y: batch_y})
+            print '{i} samples fed in, training minibatch, loss {l:.4f}, accuracy {a:.2f}%'.format(i=i*batch_size, l=loss, a=accu * 100.)
     # test the model
     accu = sess.run(accuracy, feed_dict={x: test_x, y: test_y})
     print 'test set, accuracy {a:.2f}%'.format(a=accu * 100.)
+    sess.close()
     
--- rnn/vanilla_rnn.py
+++ rnn/vanilla_rnn.py
@@ -5,22 +5,37 @@ import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
 
+
+def weight_variable_normal(shape, stddev=None):
+    if stddev is not None:
+        std = stddev
+    else:
+        std = 1.0 / np.sqrt(shape[0])
+    initial = tf.truncated_normal(shape=shape, mean=0.0, stddev=std)
+    return tf.Variable(initial)
+
+
+def weight_variable_uniform(shape, radius):
+    initial = tf.random_uniform(shape=shape, minval=-radius, maxval=radius)
+    return tf.Variable(initial)
+
+
 if __name__ == '__main__':
-    n_input = 14
-    n_step = 14
+    n_input = 28
+    n_step = 28
     n_class = 10
-    n_hidden = 2 * n_input
+    n_hidden = 4 * n_input
     batch_size = 100
-    learning_rate = 1e-3
-    n_iteration = 1e4
+    learning_rate = 1e-2
+    n_iteration = 500
     valid_steps = 1e3
 
     x = tf.placeholder(tf.float32, [None, n_step, n_input])
     y = tf.placeholder(tf.float32, [None, n_class])
-    U = tf.Variable(tf.random_normal([n_input, n_hidden]))
-    W = tf.Variable(tf.random_normal([n_hidden, n_hidden]))
+    U = weight_variable_normal([n_input, n_hidden])
+    W = weight_variable_normal([n_hidden, n_hidden])
     b = tf.Variable(np.zeros((n_hidden), dtype=np.float32))
-    V = tf.Variable(tf.random_normal([n_hidden, n_class]))
+    V = weight_variable_normal([n_hidden, n_class])
     c = tf.Variable(np.zeros((n_class), dtype=np.float32))
     
     states = []
@@ -38,11 +53,11 @@ if __name__ == '__main__':
     train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-    train_x = mnist.train.images.reshape([-1, 28, 28])[:, ::2, ::2]
+    train_x = mnist.train.images.reshape([-1, 28, 28])
     train_y = mnist.train.labels
-    valid_x = mnist.validation.images.reshape([-1, 28, 28])[:, ::2, ::2]
+    valid_x = mnist.validation.images.reshape([-1, 28, 28])
     valid_y = mnist.validation.labels
-    test_x = mnist.test.images.reshape([-1, 28, 28])[:, ::2, ::2]
+    test_x = mnist.test.images.reshape([-1, 28, 28])
     test_y = mnist.test.labels
     
     #with tf.Session() as sess:
@@ -50,15 +65,19 @@ if __name__ == '__main__':
     sess.run(tf.initialize_all_variables())
     n_sample = train_x.shape[0]
     for i in range(int(n_iteration)):
-        # validate the model
-        if i % valid_steps == 0:
-            loss, accu = sess.run([cost, accuracy], feed_dict={x: valid_x, y: valid_y})
-            print '{i} batches fed in, valid set, loss {l:.4f}, accuracy {a:.2f}%'.format(i=i, l=loss, a=accu * 100.)
+        ## validate the model
+        #if i % valid_steps == 0:
+        #    loss, accu = sess.run([cost, accuracy], feed_dict={x: valid_x, y: valid_y})
+        #    print '{i} batches fed in, valid set, loss {l:.4f}, accuracy {a:.2f}%'.format(i=i, l=loss, a=accu * 100.)
         # get a batch of samples
         idx = np.random.randint(0, n_sample, batch_size)
         batch_x = train_x[idx, :, :]
         batch_y = train_y[idx, :]
         sess.run(train_step, feed_dict={x: batch_x, y: batch_y})
+        if i % 10 == 0:
+            loss, accu, state = sess.run([cost, accuracy, states[-1]], feed_dict={x: batch_x, y: batch_y})
+            print '{i} samples fed in, training minibatch, loss {l:.4f}, accuracy {a:.2f}%'.format(i=i*batch_size, l=loss, a=accu * 100.)
     # test the model
     accu = sess.run(accuracy, feed_dict={x: test_x, y: test_y})
     print 'test set, accuracy {a:.2f}%'.format(a=accu * 100.)
+    sess.close()
